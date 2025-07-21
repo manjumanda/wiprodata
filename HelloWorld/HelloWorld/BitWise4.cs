c4518d8352b9dbfead383329ee7b67348170fcef
manjulamanda9848@gmail.com <manjulamanda9848@gmail.com>
2025-07-21 15:02:48 +0530

    first commit

---
+++ HelloWorld/HelloWorld/BitWise4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class BitWise4
+    {
+        static void Main()
+        {
+            int a = 7;
+            //0111 
+            //1000
+            Console.WriteLine(~a);//-8
+
+            int b = -6;
+            //0110
+            //1001
+            Console.WriteLine(~b);//5
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/VariableExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class VariableExample
+    {
+        static void Main()
+        {
+            int a = 12;
+            string name = "Manjula";
+            double basic = 84234.55;
+            Console.WriteLine("A value is " + a);
+            Console.WriteLine("name is " + name);
+            Console.WriteLine("Salary is " + basic);
+        }
+
+    }
+}
