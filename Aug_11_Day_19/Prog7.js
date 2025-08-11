983f6473a2e034cc17dc444b4726605e4c5e6e2f
manjumanda <manjulamanda9848@gmail.com>
2025-08-11 22:29:15 +0530

    code

---
+++ Aug_11_Day_19/Prog7.js
@@ -0,0 +1,6 @@
+var employ1={
+    empno:1,
+    name:"Manju",
+    salary:88989
+};
+console.log("Employ No ".concat(employ1.empno, "Employ Name ").concat(employ1.name, " Salary ").concat(employ1.salary, " "));
