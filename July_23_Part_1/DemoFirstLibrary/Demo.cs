706aaf2ce0996ce82322800b3c4d5c4e21dd4a90
manjumanda <manjulamanda9848@gmail.com>
2025-09-07 20:43:49 +0530

    code

---
+++ July_23_Part_1/DemoFirstLibrary/Demo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoFirstLibrary
+{
+    public class Demo
+    {
+        private string privateStr = "sanman";
+        public string publicStr = "rajesh";
+        protected string protectedStr = "daivya";
+        internal string internalstr = "gopi";
+        internal protected string internalprostr = "sailaja";
+
+        static void Main()
+        {
+            Demo demo = new Demo();
+            Console.WriteLine(demo.privateStr);
+            Console.WriteLine(demo.publicStr);
+            Console.WriteLine(demo.protectedStr);
+            Console.WriteLine(demo.internalstr);
+            Console.WriteLine(demo.internalprostr);
+
+        }
+
+    }
+}
